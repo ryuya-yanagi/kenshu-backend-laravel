99221a6c6ba1387a26f7cdf024424d6e3879c897
ryuya-yanagi <79124542+ryuya-yanagi@users.noreply.github.com>
2021-05-20 14:30:07 +0900

    update photo size

--- backend/database/factories/PhotoFactory.php
+++ backend/database/factories/PhotoFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Eloquent\Photo::class, function (Faker $faker) {
     $article_id = (new Eloquent\Article())->newQuery()->get()->random();
     return [
-        'url' => 'https://picsum.photos/seed/' . rand(1, 2000) . '/1100/500',
+        'url' => 'https://picsum.photos/seed/' . rand(1, 2000) . '/500/300',
         'article_id' => $article_id,
     ];
 });
