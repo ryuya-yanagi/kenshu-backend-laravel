6560504b5d419b890d1a6cb501568078bf6b968c
ryuya-yanagi <79124542+ryuya-yanagi@users.noreply.github.com>
2021-05-26 17:40:58 +0900

    update

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 <div class="flex-center position-ref full-height">
     <div class="content">
         <div class="title m-b-md">
-            Laravel
+            Welcome
         </div>
 
         <div class="links">
